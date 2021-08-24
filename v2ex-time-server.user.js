a286f8a9388dd5fa6f59745e78d30cbacdcbcb35
Huang Jian <huang.jian@howtimeflies.io>
2021-08-24 11:50:01 +0800

    v0.5 released.

--- v2ex-time-server.user.js
+++ v2ex-time-server.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         v2ex time saver
 // @namespace    https://github.com/HuangJian/v2ex-time-saver
-// @version      0.4
+// @version      0.5
 // @description  Save my time when browsing v2ex.com!
 // @author       ustc.hj@gmail.com
 // @match        https://www.v2ex.com/t/*
